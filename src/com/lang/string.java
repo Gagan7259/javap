77e725a86541786f0d8aa4d862c43c7c01d4876a
Gagan7259 <109654682+Gagan7259@users.noreply.github.com>
2022-11-24 14:37:16 +0530

    pk

---
+++ src/com/lang/string.java
@@ -0,0 +1,18 @@
+package com.lang;
+
+public class string {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s1 = "Gagan";
+		s1.concat("mani");
+		StringBuffer s2 = new StringBuffer("rahul");
+		s2.append("mani");
+		System.out.println(s1);
+		System.out.println(s2);
+		//System.out.println(s1.length());
+		s1.concat(s1);
+
+	}
+
+}
